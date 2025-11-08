538d164e6ca693c18fc0b1d745d9c7ff47b68594
Chris Prather <chris@prather.org>
2025-11-08 00:23:18 -0500

    Remove redundant blessed() imports from Grammar Rules
    The blessed() function is now automatically available in Perl 5.42.0
    as part of the core language, making explicit `use builtin qw(blessed);`
    imports redundant.
    
    This eliminates 13 instances of the warning:
      "state" subroutine &blessed masks earlier declaration in same scope
    
    Changes:
    - Removed `use builtin qw(blessed);` from 13 Grammar Rule files
    - No functional changes - blessed() remains available and works identically
    
    Files modified: Assignment, Block, ComparisonOp, ConcatenationOp,
    ConditionalStatement, LogicalOp, Program, RangeOp, Statement,
    StatementList, Unary, VariableDeclaration, WhileStatement

--- lib/Chalk/Grammar/Chalk/Rule/ConditionalStatement.pm
+++ lib/Chalk/Grammar/Chalk/Rule/ConditionalStatement.pm
@@ -3,7 +3,6 @@
 
 use 5.42.0;
 use experimental 'class';
-use builtin qw(blessed);
 
 class Chalk::Grammar::Chalk::Rule::ConditionalStatement :isa(Chalk::GrammarRule) {
     method evaluate($context) {
