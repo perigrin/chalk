b7765d4fa738a18221c1e4bac442a62af355d529
Chris Prather <chris@prather.org>
2025-11-25 21:02:27 -0500

    feat: add simplified IR::Node::Add2

---
+++ lib/Chalk/IR/Node/Add2.pm
@@ -0,0 +1,26 @@
+# ABOUTME: Add node - binary addition (v2 rewrite)
+# ABOUTME: Pure data node, no control edges
+use 5.42.0;
+use experimental qw(class);
+
+class Chalk::IR::Node::Add2 :isa(Chalk::IR::Node::Base2) {
+    field $left :param :reader;
+    field $right :param :reader;
+    field $id :reader = "add_" . $left->id . "_" . $right->id;
+
+    method op() { 'Add' }
+
+    method to_hash() {
+        return {
+            id     => $id,
+            op     => 'Add',
+            inputs => [$left->id, $right->id],
+            attributes => {
+                left  => $left->id,
+                right => $right->id,
+            },
+        };
+    }
+}
+
+1;
