16d33d1ca8d4b87893b2794b165053069ff89b4b
Chris Prather <chris@prather.org>
2025-12-11 15:50:19 -0500

    test: Add comprehensive tests for true/false literal parsing
    Adds both unit tests and corpus tests for true/false boolean literals
    introduced in Perl 5.36+ builtins.
    
    Unit test (t/grammar/true-false-literals.t):
    - Tests 27 different contexts where true/false should parse correctly
    - Assignment, return statements, conditionals, expressions
    - Named parameters (fat-comma): enabled => true
    - Positional parameters: $object->method(true, false)
    - Hash construction, method chaining, standalone usage
    
    Corpus tests (t/corpus/ast/):
    - true-literal.chalk/json - Simple true literal
    - false-literal.chalk/json - Simple false literal
    - true-false-positional-params.chalk/json - Method call with boolean params
    - true-false-hash-values.chalk/json - Hash with true/false values
    - true-false-conditional.chalk/json - Conditional with boolean literals
    
    Also adds generate-corpus-json.pl utility script to generate expected
    AST JSON output from .chalk input files for corpus tests.
    
    These tests ensure true/false parse without parentheses in all contexts,
    providing regression protection for grammar changes.

---
+++ generate-corpus-json.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/env perl
+# ABOUTME: Generate expected JSON AST output for corpus test .chalk files
+# ABOUTME: Parses .chalk files and writes corresponding .json expected output
+use 5.42.0;
+use experimental qw(class builtin keyword_any keyword_all);
+use utf8;
+use open qw/:std :utf8/;
+use lib 'lib';
+use JSON::PP ();
+use Chalk::Grammar::BNF;
+use Chalk::Semiring::AST;
+use Chalk::Semiring::ChalkSyntax;
+use Chalk::Semiring::Composite;
+
+my @files = @ARGV or die "Usage: $0 <file.chalk> ...\n";
+
+# Load grammar
+open my $grammar_fh, "<:utf8", "grammar/chalk.bnf" or die $!;
+my $bnf_content = do { local $/; <$grammar_fh> };
+close $grammar_fh;
+
+my $grammar = Chalk::Grammar->build_from_bnf($bnf_content, "Program", "Chalk");
+
+for my $file (@files) {
+    die "File must end in .chalk: $file\n" unless $file =~ /\.chalk$/;
+
+    # Read input
+    open my $fh, '<:utf8', $file or die "Cannot read $file: $!";
+    my $content = do { local $/; <$fh> };
+    close $fh;
+
+    # Parse to AST
+    my $chalksyntax = Chalk::Semiring::ChalkSyntax->new(grammar => $grammar);
+    my $ast = Chalk::Semiring::AST->new();
+    my $composite = Chalk::Semiring::Composite->new(
+        semirings => [$chalksyntax, $ast]
+    );
+
+    my $parser = Chalk::Parser->new(
+        grammar => $grammar,
+        semiring => $composite
+    );
+
+    my $result = $parser->parse_string($content);
+
+    unless ($result) {
+        warn "FAILED to parse: $file\n";
+        next;
+    }
+
+    # Extract AST element from composite
+    my $ast_element;
+    if ($result->can('elements')) {
+        my $elements = $result->elements;
+        $ast_element = $elements->[1];  # Second element is AST
+    } else {
+        die "Result doesn't have elements() method\n";
+    }
+
+    # Convert to JSON
+    my $ast_hash = $ast_element->to_hash();
+    my $json = JSON::PP->new->utf8->pretty->canonical->encode($ast_hash);
+
+    # Write output
+    my $json_file = $file;
+    $json_file =~ s/\.chalk$/.json/;
+
+    open my $out, '>:utf8', $json_file or die "Cannot write $json_file: $!";
+    print $out $json;
+    close $out;
+
+    print "Generated: $json_file\n";
+}
