2e62644b397fc3b2e6e4944f5a1cda638d367743
Chris Prather <chris@prather.org>
2025-12-15 16:20:47 -0500

    feat(grammar): Wire YaddaYadda to generate Die IR node
    Replace stub return with Die node creation for yada-yada operator.
    YaddaYadda now generates a Die node with "Unimplemented" message
    constant, following same control flow patterns as ReturnStatement.
    
    Add test file t/grammar/yaddayadda.t verifying Die node generation
    with proper message and control.

--- lib/Chalk/Grammar/Chalk/Rule/YaddaYadda.pm
+++ lib/Chalk/Grammar/Chalk/Rule/YaddaYadda.pm
@@ -1,20 +1,43 @@
 # ABOUTME: Semantic action for YaddaYadda - the ... (yada-yada) operator
-# ABOUTME: Generates IR node that dies with "Unimplemented" error when evaluated
+# ABOUTME: Generates Die IR node with "Unimplemented" message when evaluated
 
 use 5.42.0;
 use experimental 'class';
 
 class Chalk::Grammar::Chalk::Rule::YaddaYadda :isa(Chalk::GrammarRule) {
     method evaluate($context) {
+        use Chalk::IR::Node::Die;
+        use Chalk::IR::Node::Constant;
+        use Chalk::Grammar::Chalk::Type::Str;
+
         # YaddaYadda -> '...'
 
         # The yada-yada operator is a placeholder that dies when executed
         # In Perl, it throws: "Unimplemented at <file> line <line>"
 
-        # TODO: Build IR node for yada-yada operator
-        # This should generate a Die node with "Unimplemented" message
-        # For now, pass through the literal
-        return '...';
+        # Create "Unimplemented" constant message
+        my $message = Chalk::IR::Node::Constant->new(
+            value => 'Unimplemented',
+            type  => Chalk::Grammar::Chalk::Type::Str->new(),
+        );
+
+        # Get scope for control flow
+        my $scope = $context->env->{scope};
+        my $current_control = $scope ? $scope->current_control : undef;
+
+        # Create Die node with control and message
+        my $die_node = Chalk::IR::Node::Die->new(
+            control => $current_control,
+            message => $message,
+        );
+
+        # Kill control after die - similar to return
+        if ($scope && $scope->can('with_binding')) {
+            my $dead_scope = $scope->with_binding('$ctrl', undef);
+            $context->env->{scope} = $dead_scope;
+        }
+
+        return $die_node;
     }
 }
 
