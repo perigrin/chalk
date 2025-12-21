bb912bd4422c0a6d7db3a69417a74153df7a8730
Chris Prather <chris@prather.org>
2025-12-21 17:16:04 -0500

    feat(grammar): Implement ArrayVar and HashVar variable types (#398)
    - ArrayVar.pm: Return metadata hash with type, name, sigil (like ScalarVar)
    - HashVar.pm: Return metadata hash with type, name, sigil (like ScalarVar)
    - Variable.pm: Handle array_var and hash_var types with scope lookup
      using full variable name with sigil (@arr, %hash)
    - Added comprehensive tests for Variable rule handling all types

--- lib/Chalk/Grammar/Chalk/Rule/HashVar.pm
+++ lib/Chalk/Grammar/Chalk/Rule/HashVar.pm
@@ -1,12 +1,32 @@
-# ABOUTME: Semantic action for HashVar rule in Chalk grammar
-# ABOUTME: Passes through for HashVar constructs
+# ABOUTME: Semantic action for HashVar - extracts variable name from hash syntax
+# ABOUTME: HashVar handles %identifier and returns the variable name as metadata
 use 5.42.0;
 use experimental 'class';
 
 class Chalk::Grammar::Chalk::Rule::HashVar :isa(Chalk::GrammarRule) {
     method evaluate($context) {
-        # Pass through first child
-        return $context->child(0);
+        # HashVar -> '%' Identifier
+        # HashVar -> '%' %SPECIAL_IDENTIFIER%
+
+        # Child 0 is '%', child 1 is the identifier
+        my $identifier_node = $context->child(1);
+
+        # Extract the actual string - Identifier returns an array of characters
+        my $identifier;
+        if (ref($identifier_node) eq 'ARRAY') {
+            # Join array elements to get the identifier string
+            $identifier = join('', $identifier_node->@*);
+        } else {
+            $identifier = $identifier_node;
+        }
+
+        # Return a hashref with metadata about this variable
+        # This will be used by Variable and VariableDeclaration
+        return {
+            type => 'hash_var',
+            name => $identifier,
+            sigil => '%'
+        };
     }
 }
 
--- lib/Chalk/Grammar/Chalk/Rule/Variable.pm
+++ lib/Chalk/Grammar/Chalk/Rule/Variable.pm
@@ -7,34 +7,40 @@ use experimental 'class';
 class Chalk::Grammar::Chalk::Rule::Variable :isa(Chalk::GrammarRule) {
     method evaluate($context) {
         # Variable -> ScalarVar (lookup variable in context)
-        # Variable -> ArrayVar (TODO)
-        # Variable -> HashVar (TODO)
+        # Variable -> ArrayVar (lookup array in context)
+        # Variable -> HashVar (lookup hash in context)
         # Variable -> ArraySize (TODO)
 
-        # Get the variable metadata from child (ScalarVar)
+        # Get the variable metadata from child (ScalarVar, ArrayVar, or HashVar)
         my $var_metadata = $context->child(0);
 
-        # For now, only handle scalar variables
-        # ScalarVar returns a hashref with { type => 'scalar_var', name => $identifier, sigil => '$' }
-        if (ref($var_metadata) eq 'HASH' && $var_metadata->{type} eq 'scalar_var') {
+        # Handle all variable types that return metadata hashes
+        if (ref($var_metadata) eq 'HASH') {
+            my $var_type = $var_metadata->{type} // '';
             my $var_name = $var_metadata->{name};
+            my $sigil = $var_metadata->{sigil};
             my $scope = $context->env->{scope};
 
-            # Look up the variable's IR node from Scope
-            # Scope stores actual node objects, not just IDs
-            if ($scope) {
-                my $node = $scope->lookup($var_name);
-                if (defined($node) && ref($node) && $node->can('id')) {
-                    # Return the node object directly
-                    return $node;
-                }
-            }
+            # Construct the full variable name with sigil for scope lookup
+            my $full_name = $sigil . $var_name;
 
-            # Not yet defined - return metadata (for VariableDeclaration to extract name)
-            return $var_metadata;
+            if ($var_type eq 'scalar_var' || $var_type eq 'array_var' || $var_type eq 'hash_var') {
+                # Look up the variable's IR node from Scope
+                # Scope stores actual node objects, not just IDs
+                if ($scope) {
+                    my $node = $scope->lookup($full_name);
+                    if (defined($node) && ref($node) && $node->can('id')) {
+                        # Return the node object directly
+                        return $node;
+                    }
+                }
+
+                # Not yet defined - return metadata (for VariableDeclaration to extract name)
+                return $var_metadata;
+            }
         }
 
-        # For other variable types, pass through the metadata for now
+        # For other variable types, pass through the metadata
         return $var_metadata;
     }
 }
