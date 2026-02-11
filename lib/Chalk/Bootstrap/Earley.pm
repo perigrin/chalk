c2f3959d75de4c0790bbb35aaf2628174a26ba9d
Chris Prather <chris@prather.org>
2026-02-11 22:02:50 +0000

    refactor: Replace eval/$@ with try/catch in Earley::_complete

--- lib/Chalk/Bootstrap/Earley.pm
+++ lib/Chalk/Bootstrap/Earley.pm
@@ -271,15 +271,17 @@ class Chalk::Bootstrap::Earley {
             if (exists $chart->[$pos]->{$new_key}) {
                 # Merge with existing item using semiring add (create new item, don't mutate)
                 my $existing = $chart->[$pos]->{$new_key}->[0];
-                my $merged_value = eval { $semiring->add($existing->{value}, $new_item->{value}) };
-                if ($@) {
+                my $merged_value;
+                try {
+                    $merged_value = $semiring->add($existing->{value}, $new_item->{value});
+                } catch ($e) {
                     my $rule_name = $existing->{rule}->name();
                     my $dot = $existing->{dot};
                     my $origin = $existing->{origin};
                     my $comp_rule = $completed_item->{rule}->name();
                     my $comp_origin = $completed_item->{origin};
                     die "Ambiguity in rule '$rule_name' (dot=$dot, origin=$origin, pos=$pos) "
-                        . "completing='$comp_rule' (origin=$comp_origin): $@";
+                        . "completing='$comp_rule' (origin=$comp_origin): $e";
                 }
                 my $merged_item = $self->_make_item(
                     $existing->{rule},
